0608ce997b1be4e81f6a267aceae6b4e390488af
Kurushimunooyameru <71403286+Kurushimunooyameru@users.noreply.github.com>
2023-03-27 05:33:01 +0300

    Концовка

---
+++ Assets/Scripts/Ending.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ending : MonoBehaviour
+{
+    public int waitTime;
+    void Start()
+    {
+        StartCoroutine(WaitForLevel());
+    }
+
+    IEnumerator WaitForLevel()
+    {
+        yield return new WaitForSeconds(19);
+        SceneManager.LoadScene(1);
+    }
+  
+        
+    
+}
